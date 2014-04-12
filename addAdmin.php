b6036ea20ef16df238479722b75a4f900695d94c
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-12 16:51:40 +0530

    management stuffs

---
+++ addAdmin.php
@@ -0,0 +1,26 @@
+<?php
+	require_once("required_files/config.php");
+	require_once("classes/auth.class.php");
+	if(!isset($_COOKIE['username'],$_COOKIE['password'])){
+		header("location:adminlogin.php?error=notLoggedIn");
+		die();
+	}
+	authenticate::$db_host=$db_host;
+	authenticate::$db_user=$db_user;
+	authenticate::$db_pass=$db_pass;
+	authenticate::$db_name=$db_name;
+	authenticate::$username=$_COOKIE['username'];
+	authenticate::$password=$_COOKIE['password'];
+	$a=authenticate::check();
+	if($a!=true){
+		header("location:adminlogin.php?error=notLoggedIn");
+	}else{?>
+	<?php $title="Admin >> Add Admin"; require_once("required_files/header.php"); ?>
+	<div class="container">
+		
+	</div>
+	<?php require_once("required_files/footer.php"); ?>
+	<?php
+	}
+
+?>
