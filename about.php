dad2c41d891455e20e785d0849dbdf4c6114cea1
Rambabu <sijan@sijan-HP-Pavilion-g6-Notebook-PC.(none)>
2014-04-12 17:09:15 +0530

    initial commit for this file

---
+++ about.php
@@ -0,0 +1,37 @@
+<?php require_once('required_files/header.php'); ?>
+<div class="container">
+		<div class="row">
+			<div class="col-sm-12" style="margin:0 auto;">
+				<div class="jumbotron">
+					<p align="center" style="font-family:'acme'">About the developer</p>
+					<div class="row">
+						<!-- <div class="col-sm-6">
+						<form action="studentlogin.php" method="POST">
+							<p><input type="text" name="usn" class="form-control" placeholder="University Serial Number" style="width:400px;"></p><p><input type="password" name="password" style="width:400px;" class="form-control" placeholder="Password"></p>
+							<input type="submit" class="btn btn-default btn-login">
+						</form>
+						</div>
+						<div class="col-sm-6 text-muted" style="font-size:16px;"><span>Feel Free to submit the ratings honestly, we respect your privacy.</span>
+							<span>The information about your USN is not available to anyone unless you use a hate speech.</span>
+						</div> -->
+						<div class="col-sm-6">
+								 <div class="footer text-center">
+										      <span class="center lead ">Nishchal Gautam</span><br>
+										      <img class="img-rounded"src="https://fbcdn-photos-b-a.akamaihd.net/hphotos-ak-prn2/t1.0-0/p110x80/1385305_619517804758659_2119605174_a.jpg">
+										        <div class="pull-right">
+										          <span class="small">connect with me : </span><br>
+										          <!-- <a href="mailto:sijanshrestha2@gmail.com"><img src="img/glyphicons_010_envelope.png" alt=""></a> -->
+										          <a href="https://www.facebook.com/cyberkiller.nishchal" target="_blank"><img src="img/facebook.ico" alt=""></a>
+										          <a href="https://plus.google.com/+NishchalGautam " target="_blank"><img src="img/google.ico" alt=""></a>
+										          <a href="https://www.twitter.com/nishchal_619 " target="_blank"><img src="img/twitter.ico" alt=""></a>
+										          <a href="skype:nishchal.gautam?call" target="_blank"><img src="img/skype.ico" alt=""></a>
+										        </div>
+								 </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php require_once('required_files/footer.php'); ?>
