736b5a7e827790cb6a44eaf989d94d9f64b05dc5
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-12 16:16:48 +0530

    Seperated HTML and JavaScript

---
+++ admin.js
@@ -0,0 +1,69 @@
+	$("#membershipForm").on('submit',function(e){
+		e.preventDefault();
+		$data=$(this).serialize();
+		$.ajax({
+			url: 'ajax/addMember.php',
+			type: 'POST',
+			dataType: 'text',
+			data: $data,
+		})
+		.done(function(e) {
+			if(e!="Fail"){
+				a=$("#membershipForm").serializeArray()
+				row=$(document.createElement("tr"));
+				for (var i = 0;i<=a.length - 1; i++) {
+					data=$(document.createElement("td"));
+					data.html(a[i].value);
+					row.append(data);
+				}
+				data=$(document.createElement("td"))
+				data.html("<span class='glyphicon glyphicon-trash text-info delete' data-id='"+e+"' onClick=\"alert('You must refresh page before deleting newly added members');\" style='cursor:pointer;'> Delete</span>");
+				row.append(data)
+				table=$("table").first();
+				table.append(row);
+			}else{
+				$div=document.createElement("div");
+				$($div).addClass('alert').addClass('alert-danger').html('Something went wrong, please try again!');
+				$table=$('table').first();
+				$table.after($div);
+			}
+		})
+		.fail(function() {
+			$div=document.createElement("div");
+			$($div).addClass('alert').addClass('alert-danger').html('Seems like network is down!');
+			$table=$('table').first();
+			$table.append($div);
+		})
+		
+	})
+		$(".delete").click(function(event) {
+			$this=$(this);
+			$row=$this.parent().parent();
+			$data=$this.data("id");
+			if(confirm("This might hurt, Don't say we didn't warn you. And Deleting Can't be undone")){
+				$.ajax({
+					url: 'ajax/deleteMember.php',
+					type: 'POST',
+					dataType: 'text',
+					data: {id: $data},
+				})
+				.done(function(e) {
+					if(e=="Success"){
+						$row.hide("slow");
+					}
+					else{
+						$div=document.createElement("div");
+						$($div).addClass('alert').addClass('alert-danger').html('Please check your connection!');
+						$table=$('table').first();
+						$table.append($div);
+					}
+				})
+				.fail(function(e) {
+						$div=document.createElement("div");
+						$($div).addClass('alert').addClass('alert-danger').html('Something went wrong, please try again!');
+						$table=$('table').first();
+						$table.append($div);
+				});
+				
+			}
+		});
