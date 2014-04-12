3dfbf1d535397eb3f2ede124508c116ab238aff8
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-12 17:16:36 +0530

    changes in ownership permissions

--- about.php
+++ about.php
@@ -17,15 +17,19 @@
 						<div class="col-sm-6">
 								 <div class="footer text-center">
 										      <span class="center lead ">Nishchal Gautam</span><br>
-										      <img class="img-rounded"src="https://fbcdn-photos-b-a.akamaihd.net/hphotos-ak-prn2/t1.0-0/p110x80/1385305_619517804758659_2119605174_a.jpg">
-										        <div class="pull-right">
-										          <span class="small">connect with me : </span><br>
+										      <div class="row">
+										      	<div class="col-sm-4"><img class="img-rounded pull-right" src="https://fbcdn-photos-b-a.akamaihd.net/hphotos-ak-prn2/t1.0-0/p110x80/1385305_619517804758659_2119605174_a.jpg"></div>
+										      	<div class="col-sm-6">  <span class="small">connect with me : </span><br>
 										          <!-- <a href="mailto:sijanshrestha2@gmail.com"><img src="img/glyphicons_010_envelope.png" alt=""></a> -->
-										          <a href="https://www.facebook.com/cyberkiller.nishchal" target="_blank"><img src="img/facebook.ico" alt=""></a>
-										          <a href="https://plus.google.com/+NishchalGautam " target="_blank"><img src="img/google.ico" alt=""></a>
-										          <a href="https://www.twitter.com/nishchal_619 " target="_blank"><img src="img/twitter.ico" alt=""></a>
-										          <a href="skype:nishchal.gautam?call" target="_blank"><img src="img/skype.ico" alt=""></a>
-										        </div>
+										          <a href="https://www.facebook.com/cyberkiller.nishchal" target="_blank"><img class="social" src="img/facebook.ico" alt=""></a>
+										          <a href="https://plus.google.com/+NishchalGautam " target="_blank"><img class="social" src="img/google.ico" alt=""></a>
+										          <a href="https://www.twitter.com/nishchal_619 " target="_blank"><img class="social" src="img/twitter.ico" alt=""></a>
+										          <a href="skype:nishchal.gautam?call" target="_blank"><img class="social" src="img/skype.ico" alt=""></a></div>
+										      </div>
+										      
+										        
+										        
+										        
 								 </div>
 						</div>
 					</div>
