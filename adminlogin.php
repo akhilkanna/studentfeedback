40989c10c9aeb1ed614d5f9f2330725fd58e18a7
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-12 11:55:32 +0530

    admin login functional, redirect admin.php when password is correct

--- adminlogin.php
+++ adminlogin.php
@@ -1,22 +1,63 @@
 <?php 
 if(isset($_POST['submit']))
 {
-	if((!isset($_POST['usn'],$_POST['password'])) || $_POST['usn']=='' || $_POST['password']==''){
+	if((!isset($_POST['username'],$_POST['password'])) || $_POST['username']=='' || $_POST['password']==''){
 		header("location:studentlogin.php?error=emptyFields");
 	}
 	//login logic.
+	$username=$_POST['username'];
+	$password=$_POST['password'];
+	require_once("required_files/config.php");
+	$con=mysql_connect($db_host,$db_user,$db_pass);
+	$username=mysql_real_escape_string($username);
+	$sql="SELECT * FROM `$db_name`.`admin` WHERE `username`='$username' LIMIT 1";
+	$query=mysql_query($sql);
+	$num=mysql_num_rows($query);
+	if($num!=1){
+		header("location:adminlogin.php?error=noUser");
+		die();
+	}
+	$data=mysql_fetch_object($query);
+	$passdb=$data->password;
+	$password=crypt($password,$salt);
+	if($password==$passdb){
+		//set to cookie
+		setcookie("username",$_POST['username'],time()+3600,"/");
+		setcookie("password",$passdb,time()+3600,"/");
+		header("location:admin.php");
+	}else{
+		header("location:adminlogin.php?error=passwordMismatch");
+	}
 
 }else{
 ?>
 <?php require_once('required_files/header.php'); ?>
 	<div class="jumbotron">
-		<p align="center" style="font-family:'acme'">Login using your University Serial Number</p>
 		<?php if(isset($_GET['error'])){
+			$error=$_GET['error'];
+
 		?>	
-		<div class="alert alert-danger col-sm-6 col-sm-offset-3" align="center"><?php if($_GET['error']=="emptyFields"){
-			echo "Form was not filled completely!";
-			}else{echo "Username and password mismatch";
-			} ?>
+		<div class="alert alert-danger col-sm-6 col-sm-offset-3" align="center">
+		<?php 
+			switch ($error) {
+				case 'emptyFields':
+					echo "Form was not filled completely!";
+					break;
+				case 'noUser':
+					echo "No such user is registered";
+					break;
+				case 'passwordMismatch':
+					echo "Sorry, we don't recognize the credentials";
+					break;
+				case 'notLoggedIn':
+					echo "You might wanna login first before you view admin page.";
+					break;
+				default:
+					echo "Some Error occured! Please try again";
+					break;
+			}
+
+ ?>
 		</div>
 		<?php
 			} ?>
