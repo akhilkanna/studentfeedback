ea6ccd54d278d8612b0c27b1f4a037f3d6e56cf5
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-14 00:20:31 +0530

    added the functionality to save time and determine the semester user is in

--- studentlogin.php
+++ studentlogin.php
@@ -13,6 +13,8 @@ if(isset($_POST['submit']))
 	$usn=strtolower($_POST['usn']);
 	$password=strtolower($_POST['password']);
 	if($usn==$password){
+		setcookie("sem",$_POST['sem'],time()+3600,"/");
+		setcookie("sec",$_POST['sec'],time()+3600,"/");
 		setcookie("usn", $usn, time()+35000, "/");
 		require_once("required_files/config.php");
 		$usn=mysql_real_escape_string($usn);
@@ -41,13 +43,73 @@ if(isset($_POST['submit']))
 			<div class="row">
 				<div class="col-sm-6 col-sm-offset-3">
 				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-					<p><input type="text" name="usn" class="form-control" placeholder="University Seat Number" style="width:400px;"></p><p><input type="password" name="password" style="width:400px;" class="form-control" placeholder="Password"></p>
-					<input type="submit" class="btn btn-default btn-login" name="submit">
+					<p><input type="text" id="usn" name="usn" onchange="setSem()" class="form-control" placeholder="University Seat Number" style="width:400px;"></p><p><input type="password" name="password" style="width:400px;" class="form-control" placeholder="Password"></p>
+					<p>Semester:<select name="sem" id="semester" class="select form-control" style="width:400px;">
+						<option>2</option>
+						<option>4</option>
+						<option>6</option>
+						<option>8</option>
+					</select></p>
+					<p>Section:
+						<select name="sec" id="sec" class="select form-control" style="width:400px;">
+							<option value="A">A</option>
+							<option value="B">B</option>
+						</select>
+					</p>
+					<p>Branch:
+						<select name="branch" id="branch" class="select form-control" disabled style="width:400px;">
+							<option>ISE</option>
+							<option>CSE</option>
+							<option>ECE</option>
+							<option>EEE</option>
+							<option>CIV</option>
+							<option>ME</option>
+						</select>
+					</p>
+					<input type="submit" class="btn btn-default btn-login" value="Log me in!" name="submit">
 				</form>
 				</div>
 			</div>
 		</form>
 	</div>
+	<script>
+	function setSem () {
+		setBranch();
+		$.ajax({
+			url: 'ajax/getSem.php',
+			type: 'GET',
+			dataType: 'text',
+			data: {usn: $("#usn")[0].value},
+		})
+		.done(function(e) {
+			if(e!="fail"){
+				$("#semester")[0].value=e;
+			}else{
+				return 0;
+			}
+		})
+		.fail(function() {
+			console.log("error");
+		});
+		
+	}
+		function setBranch () {
+		$.ajax({
+			url: 'ajax/getBranch.php',
+			type: 'GET',
+			dataType: 'text',
+			data: {usn: $("#usn")[0].value},
+		})
+		.done(function(e) {
+			if(e!="fail"){
+				$("#branch")[0].value=e;
+			}else{
+				return 0;
+			}
+		});
+		
+	}
+	</script>
 <?php require_once('required_files/footer.php'); ?>
 <?php
 }
