a748944e4f49ea5aa4972eee139dcbad622f7a96
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-14 00:23:17 +0530

    minor changes may be

--- feedback.php
+++ feedback.php
@@ -1,8 +1,4 @@
 <?php require_once("required_files/header.php"); ?>
 
-	<table class="table">
-		<tr> <th></th> <th></th> <th></th> <th></th> <th></th> <th></th> <th></th> <th></th> <th></th> </tr>
-		
-	</table>
 
 <?php require_once("required_files/footer.php"); ?>
