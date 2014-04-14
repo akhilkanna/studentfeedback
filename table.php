afe3fee482e28c484af1530606db258dadd3496c
Rambabu <sijan@sijanPC.(none)>
2014-04-14 17:12:41 +0530

    markup for form2

--- table.php
+++ table.php
@@ -1,15 +1,55 @@
-<?php 
-$teachers = array(1,2,3,4,5,6,7,8);
-$parameters = array('blackboard usage','audibility','able to clear doubts','preparation for exam');
-?>
-<table>
-	<tr>
-		<?php 
-		$a="<th>";	
-		$a=$a. implode("</th><th>", $teachers);
-		$a=$a. "</th>";
-		echo $a;
-		?>
-	</tr>
-
-</table>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<table class="tg">
+  <tr>
+    <th class="tg-031e" rowspan="3">Class Room Ambience</th>
+    <td class="tg-031e">star</th>
+    <td class="tg-031e"></th>
+  </tr>
+  <tr>
+    <td class="tg-031e">Power Supply</td>
+    <td class="tg-031e">star</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">BackUp</td>
+    <td class="tg-031e">star</td>
+  </tr>
+  <tr>
+    <th class="tg-031e" rowspan="3">Cleanliness</td>
+    <td class="tg-031e">Classroom</td>
+    <td class="tg-031e">star</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Corridoors</td>
+    <td class="tg-031e">star</td>
+  </tr>
+  <tr>
+    <td class="tg-031e">Toilets</td>
+    <td class="tg-031e">star</td>
+  </tr>
+  <tr>
+  	<td class="tg-031e">star</th>
+    <th class="tg-031e" >Water supply</th>
+    <td class="tg-031e">star</th>
+   </tr>
+   <tr>
+   	<td class="tg-031e">star</th>
+    <th class="tg-031e" >Campus Environment</th>
+    <td class="tg-031e">star</th>
+   </tr>
+   <tr>
+   	<td class="tg-031e">star</th>
+    <th class="tg-031e" >Medical Facilites IN Emergency</th>
+    <td class="tg-031e">star</th>
+   </tr>
+   <tr>
+    <th class="tg-031e" rowspan="3">Transport</th>
+    <td class="tg-031e">star</th>
+   </tr> 	
+</table></body>
+</html>
