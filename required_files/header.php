5218088dfc8812cc0e5c5c79ad308342e282de00
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-15 00:18:12 +0530

    referencing stylesheet and JS with respect to document root

--- required_files/header.php
+++ required_files/header.php
@@ -4,21 +4,21 @@
 	<meta charset="UTF-8">
 	<title><?php $val= isset($title)? $title: "Feedback form"; echo $val;?></title>
 	<link rel="icon" type="shortcut-icon" href="feedback.jpg">
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/styles.css">
 	<script type="text/javascript" language="javascript" src="//cdn/jquery.js"></script>
-	<script type="text/javascript" language="javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" language="javascript" src="/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<div class="head">Feedback Form 3</div>
 	<div class="head subtitle">Login</div>
 	<nav>
 		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="about.php" class="about">About Feedback</a></li>
-			<li><a href="adminlogin.php" class="admin">Admin</a></li>
-			<li><a href="studentlogin.php" class="login">Login</a></li>
-			<li><a href="admin/" class="result">Results</a></li>
+			<li><a href="/index.php">Home</a></li>
+			<li><a href="/about.php" class="about">About Feedback</a></li>
+			<li><a href="/adminlogin.php" class="admin">Admin</a></li>
+			<li><a href="/studentlogin.php" class="login">Login</a></li>
+			<li><a href="/admin/" class="result">Results</a></li>
 			<li><a href="https://www.github.com/cyberhck/studentfeedback" class="source">Source on Github</a></li>
 		</ul>
 	</nav>
