c4ecfb0016cb2365269a4c11a36adee337594e6e
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-14 19:42:02 +0530

    image to the right, markup changes, indentation proper

--- about.php
+++ about.php
@@ -1,47 +1,35 @@
 <?php require_once('required_files/header.php'); ?>
 <div class="container">
-		<div class="row">
-			<div class="col-sm-12" style="margin:0 auto;">
-				<div class="jumbotron">
-					<p align="center" style="font-family:'acme'">About developers</p>
-					<div class="row">
-						<!-- <div class="col-sm-6">
-						<form action="studentlogin.php" method="POST">
-							<p><input type="text" name="usn" class="form-control" placeholder="University Serial Number" style="width:400px;"></p><p><input type="password" name="password" style="width:400px;" class="form-control" placeholder="Password"></p>
-							<input type="submit" class="btn btn-default btn-login">
-						</form>
+	<div class="row">
+		<div class="col-sm-12" style="margin:0 auto;">
+			<div class="jumbotron">
+				<p align="center" style="font-family:'acme'">About developers</p>
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="footer text-center">
+							<span class="center lead">Nishchal Gautam</span><br>
+							<div class="row">
+								<div class="col-sm-6">
+									Passionate Web Developer<br>
+									Enthusiastic Entrepreneur<br>
+									<blockquote class="blockquote blockquote-reverse"><p>When Technology Becomes Passion</p></blockquote>
+									<span class="small">connect with me : </span><br>
+									<br><a href="https://www.facebook.com/cyberkiller.nishchal" target="_blank"><img class="social" src="img/facebook.ico" alt=""></a>
+								    <a href="https://plus.google.com/+NishchalGautam " target="_blank"><img class="social" src="img/google.ico" alt=""></a>
+								    <a href="https://www.twitter.com/nishchal_619 " target="_blank"><img class="social" src="img/twitter.ico" alt=""></a>
+								    <a href="skype:nishchal.gautam?call" target="_blank"><img class="social" src="img/skype.ico" alt=""></a>
+								</div>
+								<div class="col-sm-6"><img class="img-circle pull-right" src="img/nishchal.jpg"></div>
+							</div>
 						</div>
-						<div class="col-sm-6 text-muted" style="font-size:16px;"><span>Feel Free to submit the ratings honestly, we respect your privacy.</span>
-							<span>The information about your USN is not available to anyone unless you use a hate speech.</span>
-						</div> -->
-						<div class="col-sm-6">
-								 <div class="footer text-center">
-										      <span class="center lead">Nishchal Gautam</span><br>
-										      <div class="row>"
-										      	<div class="col-sm-6"><img class="img-circle pull-right" src="img/nishchal.jpg"></div>
-										      	<div class="col-sm-6">  
-										      		Passionate Web Developer<br>
-										      		Enthusiastic Entrepreneur<br>
-										      		<blockquote class="blockquote blockquote-reverse"><p>When Technology Becomes Passion</p></blockquote>
-										      		<span class="small">connect with me : </span><br>
-
-										      		
-										          <!-- <a href="mailto:sijanshrestha2@gmail.com"><img src="img/glyphicons_010_envelope.png" alt=""></a> -->
-										          <br><a href="https://www.facebook.com/cyberkiller.nishchal" target="_blank"><img class="social" src="img/facebook.ico" alt=""></a>
-										          <a href="https://plus.google.com/+NishchalGautam " target="_blank"><img class="social" src="img/google.ico" alt=""></a>
-										          <a href="https://www.twitter.com/nishchal_619 " target="_blank"><img class="social" src="img/twitter.ico" alt=""></a>
-										          <a href="skype:nishchal.gautam?call" target="_blank"><img class="social" src="img/skype.ico" alt=""></a></div>
-										      </div>
-										      
-										        
-										        
-										        
-								 </div>
-						</div>
+					</div>
+					<div class="col-sm-6">
+						right container
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>			
 
 <?php require_once('required_files/footer.php'); ?>
