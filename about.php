96a932468d7392ccfc71267abcac44a291e3e0c4
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-16 10:37:15 +0530

    little modifications

--- about.php
+++ about.php
@@ -9,37 +9,40 @@
 					<div class="row">
 						<div class="col-sm-6">
 							<div class="footer text-center">
-								<span class="center lead">Nishchal Gautam</span><br>
+								<p class="text-center lead">Nishchal Gautam</p><br>
 								<div class="row">
 									<div class="col-sm-6">
-										Passionate Web Developer<br>
+										<small>Passionate Web Developer</small><br>
 										Enthusiastic Entrepreneur<br>
-										<blockquote class="blockquote blockquote-reverse"><p>When Technology Becomes Passion</p></blockquote>
+										<blockquote class="blockquote blockquote-reverse"><p class="text-center">"If you control the code, you control the world"</p></blockquote><br><br>
 										<span class="small">connect with me : </span><br>
 										<br><a href="https://www.facebook.com/cyberkiller.nishchal" target="_blank"><img class="social" src="img/facebook.ico" alt=""></a>
 									    <a href="https://plus.google.com/+NishchalGautam " target="_blank"><img class="social" src="img/google.ico" alt=""></a>
 									    <a href="https://www.twitter.com/nishchal_619 " target="_blank"><img class="social" src="img/twitter.ico" alt=""></a>
 									    <a href="skype:nishchal.gautam?call" target="_blank"><img class="social" src="img/skype.ico" alt=""></a>
 									</div>
-									<div class="col-sm-6"><img class="img-circle pull-right" style="width:210px;height:210px" src="img/nishchal.jpg"></div>
+									<div class="col-sm-6"><img class="img-circle pull-right" src="img/nishchal.jpg"></div>
 								</div>
 							</div>
 						</div>
 						<div class="col-sm-6">
-							<span class="center lead">Sijan Shrestha</span><br>
+								<p class="text-center lead">Sijan Shrestha</p><br>
 								<div class="row">
-									<div class="col-sm-6"><img style="width:210;height:210;" src="" class="img-circle"></div>
+									<div class="col-sm-6"><img class="img-circle pull-right " height="200" width="200" src="img/sijan.jpg"></div>
 									<div class="col-sm-6">
 										Passionate coder<br>
+										Debugging Ninja <br>
 										<br>
-										<blockquote class="blockquote blockquote-reverse"><p>Technology is what matters </p></blockquote>
-										<span class="small">connect with me : </span><br>
+										<blockquote class="blockquote "><p class="text-center">"Dont document The problem Fix it !" </p></blockquote>
+										<span class="small">connect with me :</span><br>
 										<br><a href="https://www.facebook.com/sijanshs" target="_blank"><img class="social" src="img/facebook.ico" alt=""></a>
-									    <a href="https://plus.google.com/sijanshrestha" target="_blank"><img class="social" src="img/google.ico" alt=""></a>
-									    <a href="https://www.twitter.com/sijan" target="_blank"><img class="social" src="img/twitter.ico" alt=""></a>
+									    <a href="https://plus.google.com/u/0/+SijanShresthaNepal" target="_blank"><img class="social" src="img/google.ico" alt=""></a>
+									    <a href="https://twitter.com/sijan_shrestha" target="_blank"><img class="social" src="img/twitter.ico" alt=""></a>
 									    <a href="skype:sidzanshrestha?call" target="_blank"><img class="social" src="img/skype.ico" alt=""></a>
 									</div>
+									
 								</div>
+
 						</div>
 					</div>
 				</div>
