dfa5c870b6a159daca5130b0fda76307c3bbf8c4
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-16 16:13:59 +0530

    sijan's stuffs

--- about.php
+++ about.php
@@ -21,19 +21,19 @@
 									    <a href="https://www.twitter.com/nishchal_619 " target="_blank"><img class="social" src="img/twitter.ico" alt=""></a>
 									    <a href="skype:nishchal.gautam?call" target="_blank"><img class="social" src="img/skype.ico" alt=""></a>
 									</div>
-									<div class="col-sm-6"><img class="img-circle pull-right" src="img/nishchal.jpg"></div>
+									<div class="col-sm-6"><img class="img-circle pull-right" style="height:200px;width:200px;" src="img/nishchal.jpg"></div>
 								</div>
 							</div>
 						</div>
-						<div class="col-sm-6">
+						<div class="col-sm-6 text-center">
 								<p class="text-center lead">Sijan Shrestha</p><br>
 								<div class="row">
 									<div class="col-sm-6"><img class="img-circle pull-right " height="200" width="200" src="img/sijan.jpg"></div>
-									<div class="col-sm-6">
+									<div class="col-sm-6 ">
 										Passionate coder<br>
 										Debugging Ninja <br>
-										<br>
-										<blockquote class="blockquote "><p class="text-center">"Dont document The problem Fix it !" </p></blockquote>
+									
+										<blockquote class="blockquote "><p class="text-center">"Dont document The problem Fix it!"</p></blockquote>
 										<span class="small">connect with me :</span><br>
 										<br><a href="https://www.facebook.com/sijanshs" target="_blank"><img class="social" src="img/facebook.ico" alt=""></a>
 									    <a href="https://plus.google.com/u/0/+SijanShresthaNepal" target="_blank"><img class="social" src="img/google.ico" alt=""></a>
--- required_files/footer.php
+++ required_files/footer.php
@@ -1,3 +1,3 @@
-<footer align="center">Feedback form &copy; Nishchal Gautam submitted to SVCE. Designed, Markup and Backend by <a href="http://nishgtm.com">Nishchal Gautam</a></footer>
+<footer align="center">Feedback form &copy; Nishchal Gautam submitted to SVCE. Designed by <a href="https://www.facebook.com/sijanshs">Sijan Shrestha</a>, Markup and Backend by <a href="http://nishgtm.com">Nishchal Gautam</a></footer>
 </body>
 </html>
