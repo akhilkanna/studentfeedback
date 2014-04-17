a982d0b7fc803eb5cd00757a96607b243bc135fb
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-04-17 21:14:20 +0530

    proper redirection

--- logout.php
+++ logout.php
@@ -1,11 +1,12 @@
-<?php 
+<?php
+if(isset($_COOKIE['usn'])){
+	setcookie("usn","",time()+3600);
+	header("location:about.php");
+	die();
+}
 if(isset($_COOKIE['password'])){
 	setcookie("password","",time()-3600);
 	header("location:adminlogin.php");
 	die();
-}
-if(isset($_COOKIE['usn'])){
-	setcookie("usn","",time()+3600);
-	header("location:about.php");
 }
 ?>
