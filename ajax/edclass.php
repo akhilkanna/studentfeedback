a984d7e9571216637eb0fb8909d82281af6a1739
Nishchal Gautam <cyberhck@users.noreply.github.com>
2014-05-09 17:42:02 +0530

    Added Authentication

--- ajax/edclass.php
+++ ajax/edclass.php
@@ -1,5 +1,22 @@
 <?php
 	require_once("../required_files/config.php");
+	require_once("../classes/auth.class.php");
+	if(!isset($_COOKIE['username'],$_COOKIE['password'])){
+		header("location:adminlogin.php?error=notLoggedIn");
+		die();
+	}
+	authenticate::$db_host=$db_host;
+	authenticate::$db_user=$db_user;
+	authenticate::$db_pass=$db_pass;
+	authenticate::$db_name=$db_name;
+	authenticate::$username=$_COOKIE['username'];
+	authenticate::$password=$_COOKIE['password'];
+	$a=authenticate::check();
+	if(!$a){
+		echo "Fail";
+		header("location:adminlogin.php?error=notLoggedIn");
+	}else{
+	require_once("../required_files/config.php");
 	$con=mysql_connect($db_host,$db_user,$db_pass);
 	$check=$_POST['enable'];
 	$value=$_POST['values'];
@@ -14,4 +31,5 @@
 	}else{
 		echo "fail".mysql_error();
 	}
+}
 ?>
