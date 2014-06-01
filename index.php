c6d5f01c487ee67f60a10fff996c7b5cd40679b9
Nishchal Gautam <cyberhck@users.noreply.github.com>
2014-06-01 06:10:05 +0530

    Update for viewing in gh-pages

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
-<?php $title="Welcome! Feedback form 3"; require_once('required_files/header.php'); ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php $val= isset($title)? $title: "Feedback form"; echo $val;?></title>
+	<link rel="icon" type="shortcut-icon" href="feedback.jpg">
+	<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/styles.css">
+	<script type="text/javascript" language="javascript" src="/jquery.js"></script>
+	<script type="text/javascript" language="javascript" src="/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="head">Feedback Form 3</div>
+	<div class="head subtitle">Login</div>
+	<nav>
+		<ul>
+			<li><a href="/index.php" class="home">Home</a></li>
+			<li><a href="/about.php" class="about">About Feedback</a></li>
+			<li><a href="/adminlogin.php" class="admin">Admin</a></li>
+			<li><a href="/studentlogin.php" class="login">Student</a></li>
+			<li><a href="/admin/" class="result">Results</a></li>
+			<li><a href="https://www.github.com/cyberhck/studentfeedback" class="source">Source on Github</a></li>
+		</ul>
+	</nav>
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12" style="margin:0 auto;">
@@ -14,4 +37,6 @@
 			</div>
 		</div>
 	</div>
-<?php require_once('required_files/footer.php'); ?>
+	<footer align="center">Feedback form &copy; Nishchal Gautam submitted to SVCE. Designed by <a href="https://www.facebook.com/sijanshs">Sijan Shrestha</a>, Markup and Backend by <a href="http://nishgtm.com">Nishchal Gautam</a></footer>
+</body>
+</html>
