a3ca0ca66b21bce4a23037665e6ffaee3fd18b2e
Nishchal Gautam <gautam.nishchal@gmail.com>
2014-10-14 12:21:53 +0530

    minor changes

--- admin/getreport.php
+++ admin/getreport.php
@@ -41,10 +41,10 @@
 				</select>
 				<p>
 					<select name="sem" class="form-control" style="width:300px;">
-						<option value="2">2</option>
-						<option value="4">4</option>
-						<option value="6">6</option>
-						<option value="8">8</option>
+						<option value="1">1</option>
+						<option value="3">3</option>
+						<option value="5">5</option>
+						<option value="7">7</option>
 					</select>
 				<p>
 					<select name="section" class="form-control" style="width:300px;">
